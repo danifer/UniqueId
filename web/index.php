dea8c9b7067461f8da2a7ea71034ae47ec52a8c3
Keeton Harrington <keeton@danifer.com>
2012-06-06 12:37:19 -0500

    adding frontend controllers

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
-
+require_once(dirname(__FILE__).'/../lib/system.php');
+require_once(dirname(__FILE__).'/../lib/debug.php');
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
